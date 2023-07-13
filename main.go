1dd2fc8c955bcee37021da521b98a20dee3045a2
gabriel <gabrieldennis17@gmail.com>
2023-07-13 21:16:29 +1000

    feat: updating readme

---
+++ main.go
@@ -0,0 +1,27 @@
+// Package main
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/den173/geotiff/geotiff"
+)
+
+func main() {
+	f, err := os.Open("geotiff/testdata/WCSServer.tif")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	// read the geotiff
+	gtiff, _ := geotiff.Read(f)
+
+	// get the geotiff bounds
+	bounds, err := gtiff.Bounds()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(bounds)
+}
